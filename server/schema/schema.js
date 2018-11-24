3a0db3e53a742e7cfbe913bab338832d4d30f185
Saurabh Bayani <saurabh.bayani@gmail.com>
2018-11-25 00:06:01 +0530

    Adding Director dummy data and Schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,13 @@
 const graphql = require('graphql');
 const _ =  require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+} = graphql;
 
 // Dummy data
 const moviesList = [
@@ -10,17 +16,35 @@ const moviesList = [
     {"id":"3","name":"Impromptu","genre":"Comedy|Romance"},
     {"id":"4","name":"Right Now (À tout de suite)","genre":"Crime|Drama|Romance|Thriller"},
     {"id":"5","name":"Young & Wild (Joven y alocada)","genre":"Drama"}
-]
+];
+
+const directorsList = [
+    {"id":1,"name":"Ritchie Breston","gender":"Male"},
+    {"id":2,"name":"Bald Shrieve","gender":"Male"},
+    {"id":3,"name":"Maxie Severns","gender":"Male"},
+    {"id":4,"name":"Alisha Bambery","gender":"Female"},
+    {"id":5,"name":"Freda Colyer","gender":"Female"}
+];
 
 // Schema for movies
 const MovieType = new GraphQLObjectType({
     name: 'MovieType',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
 });
+
+// Schema for directors
+const DirectorType =  new GraphQLObjectType({
+    name: 'DirectorType',
+    fields: ()=>({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        gender: {type: GraphQLString}
+    })
+})
 
 const RootQueryType = new GraphQLObjectType({
     name: 'RootQueryType',
